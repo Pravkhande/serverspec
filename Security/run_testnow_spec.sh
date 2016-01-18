5ae717f4005557de408080124092de63d4f58b96
Nikhita <nikhita.dhawane@opexsoftware.com>
2016-01-18 15:47:04 +0530

    Added copy logic for report

--- Security/run_testnow_spec.sh
+++ Security/run_testnow_spec.sh
@@ -6,7 +6,11 @@ export HOST_ROLE="$ROLE"
 
 rake spec:"$HOST_ROLE"
 
-scp  -o StrictHostKeyChecking=no -r -i $mainDir/TestNow.key ${mainDir}/code/Security/reports/${SPEC_HOST_NAME}* ubuntu@$WEB_SERVER_IP:$destOutPut
+#destOutPut="/var/www/html/${JOB_ID}/${RUN_ID}"
+#
+#scp  -o StrictHostKeyChecking=no -r -i $WORKING_DIR/TestNow.key ${WORKING_DIR}/code/Security/reports/${SPEC_HOST_NAME}* ubuntu@$WEB_SERVER_IP:$destOutPut
+
+
 echo "********************************************************************************************"
 echo "* Report for this execution has been created in Reports folder as 'serverspec_report.html' *"
 echo "********************************************************************************************"
