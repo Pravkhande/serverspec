14230662be06ec99cc2deadf122a173cb3a60c4c
Nikhita <nikhita.dhawane@opexsoftware.com>
2017-05-12 05:14:34 +0000

    Update utilities.rb

--- Security/spec/utilities.rb
+++ Security/spec/utilities.rb
@@ -1,7 +1,7 @@
 def replace_report_title
   ENV['TITLE']="SECURITY AUTOMATION REPORT" if ENV['TITLE'].nil?
-   # report_file = File.absolute_path("#{ENV['SPEC_HOST_NAME']}_#{ENV['ROLE']}.html","reports")
-   report_file = File.absolute_path("#{ENV['SPEC_HOST_NAME']}_security_report.html","reports")
+   report_file = File.absolute_path("#{ENV['SPEC_HOST_NAME']}_#{ENV['ROLE']}.html","reports")
+   #report_file = File.absolute_path("#{ENV['SPEC_HOST_NAME']}_security_report.html","reports")
   puts report_file
   doc = File.read(report_file)
   new_doc = doc.sub("RSpec Code Examples", "#{ENV['TITLE']}")
